cf724715b3b5a29e66959fd34eeee4974e9baf07
Luis Gomes <105812422+chicosbg@users.noreply.github.com>
2024-11-21 21:34:58 -0300

    add escrita de logs

--- src/main.py
+++ src/main.py
@@ -1,29 +1,26 @@
-from utils.loggerUtils import LoggerUtils
 from models.Processo import Processo
 from models.Interrupcao import Interrupcao
 from models.OS import OS
 
 from rich.console import Console
-from rich.table import Table
-from rich.live import Live
 from rich.panel import Panel
-from rich.progress import Progress, SpinnerColumn, BarColumn, TextColumn
 
 import random
-import time
-import shutil
 import signal
-import os
+import time
 
 def handle_sigint(signum, frame):
         print("\nEncerrando sistema...")
+        a = console.export_text(clear=True, styles=False)
+        
+        with open(f"./logs/log{time.time().as_integer_ratio()[0]}.txt", "w") as file:
+             file.write(a)
         exit(0)
 
-console = Console()
-
 if __name__ == "__main__":
+    console = Console(record=True)
     processo = Processo.Processo()
-    os = OS.OS()
+    OS = OS.OS()
 
     signal.signal(signal.SIGINT, handle_sigint)
 
@@ -39,7 +36,7 @@ if __name__ == "__main__":
     )
     console.print(Panel.fit(painel_processo, title="Sistema Operacional", border_style="blue"))
     ######## Imprime contexto do processo #########
-
+    
     input()
 
     while True:
@@ -49,10 +46,10 @@ if __name__ == "__main__":
             console.log(f"[bold yellow]! Nova interrupção ![/bold yellow]")
             if not processo.interrompido:
                 console.log(f"[green] Contexto salvo pelo sistema operacional: tempo de execução = {processo.tempo}, pc = {hex(processo.pc)}, ponteiro da pilha = {hex(processo.ponteiro_pilha)}[/green]")
-                os.salva_contexto(processo)
+                OS.salva_contexto(processo)
                 processo.interrompe()
 
-            os.adicionar_na_fila_de_interrupcoes(Interrupcao.Interrupcao())
+            OS.adicionar_na_fila_de_interrupcoes(Interrupcao.Interrupcao())
 
         if not processo.interrompido:   # Se não houver interrupções executa normalmente
             processo.executa()
@@ -72,7 +69,7 @@ if __name__ == "__main__":
 
 
         else:                       # Se houver, tratam-se as interrupções
-            interrupcao = os.fila_de_interrupcoes[0]
+            interrupcao = OS.fila_de_interrupcoes[0]
             ######## Imprime interrupção sendo tratada #########
             interrupcao_atual = f"Interrupção - {interrupcao.tipo}"
             painel_interrupcao = Panel(
@@ -86,11 +83,11 @@ if __name__ == "__main__":
             console.print(Panel.fit(painel_interrupcao, title="Sistema Operacional", border_style="blue"))
             ######## Imprime interrupção sendo tratada #########
 
-            os.tratar_interrupcao()
+            OS.tratar_interrupcao()
 
 
-        if(len(os.fila_de_interrupcoes) == 0 and processo.interrompido == True):
-            processo.retomar(os.tempo_salvo, os.pc_salvo, os.ponteiro_pilha_salvo)
+        if(len(OS.fila_de_interrupcoes) == 0 and processo.interrompido == True):
+            processo.retomar(OS.tempo_salvo, OS.pc_salvo, OS.ponteiro_pilha_salvo)
 
             ######## Imprime contexto do processo #########
             processo_atual = "Processo Principal"
