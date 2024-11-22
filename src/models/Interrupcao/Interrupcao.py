9a3c9d894141d5d8b0f5ecfe97e43a5c352c1d2f
Luis Gomes <105812422+chicosbg@users.noreply.github.com>
2024-11-21 21:35:42 -0300

    fix: prioridades

--- src/models/Interrupcao/Interrupcao.py
+++ src/models/Interrupcao/Interrupcao.py
@@ -2,25 +2,7 @@ import random
 
 class Interrupcao:
     def __init__(self) -> None:
-        escolha_aleatoria = random.choice(["teclado", "impressora", "disco"])
-        self.tipo = escolha_aleatoria
-        self.prioridade = 0
+        escolha_aleatoria = random.choice([{"tipo":"teclado", "prioridade": 1}, {"tipo":"impressora", "prioridade": 2}, {"tipo":"disco", "prioridade": 3}])
+        self.tipo = escolha_aleatoria["tipo"]
+        self.prioridade = escolha_aleatoria["prioridade"]
         self.tempo_execucao = random.randint(1, 7)
-
-    # def tratar(self):
-    #     if self.tipo == "teclado":
-    #         self.trata_interrupcao_teclado()
-    #     elif self.tipo == "impressora":
-    #         self.trata_interrupcao_impressora()
-    #     elif self.tipo == "disco":
-    #         self.trata_interrupcao_disco()
-    #     self.tempo_execucao -= 1
-
-    # def trata_interrupcao_teclado(self):
-    #     print("Tratando interrupção teclado...")
-
-    # def trata_interrupcao_impressora(self):
-    #     print("Tratando interrupção impressora...")
-
-    # def trata_interrupcao_disco(self):
-    #     print("Tratando interrupção disco...")
